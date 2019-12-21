6e34776d6d35371b2d9afd64a6097b69bccad391
sherpal8 <sherpalkorotana@icloud.com>
2019-12-21 20:38:00 +0000

    devServer configuration of webpack

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -15,5 +15,15 @@ module.exports = {
     path: path.resolve(__dirname, "build"),
     publicPath: "/",
     filename: "bundle.js"
+  },
+  devServer: {
+    //settings to serve the App locally in dev mode using webpack
+    stats: "minimal", // minimise output in console so not much noise when running
+    overlay: true, // overlay errors in browser
+    historyApiFallback: true, // all request sent to index.html. Deeplinks handled by React-Router
+    // last 3 entries below to offset open-issue between webpack and latest version of chrome
+    disableHostCheck: true,
+    headers: { "Access-Control-Allow-Origin": "*" },
+    https: false
   }
 };
