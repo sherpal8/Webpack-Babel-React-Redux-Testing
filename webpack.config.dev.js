41377466c078bdf68247ce2692cc500337bcdff7
sherpal8 <sherpalkorotana@icloud.com>
2019-12-21 21:22:29 +0000

    rectify error to property name

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -7,7 +7,7 @@ process.env.NODE_ENV = "development";
 module.exports = {
   mode: "development", // disable production only features
   target: "web", // the target app is for the browser
-  devTool: "cheap-module-source-map", // source map debugging to view code in browser
+  devtool: "cheap-module-source-map", // source map debugging to view code in browser
   entry: "./src/index",
   output: {
     // there is no output in 'dev' as stored in RAM memory, but
