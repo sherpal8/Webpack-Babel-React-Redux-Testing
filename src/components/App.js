c5f41687cc895c47dc7a0f2dc2c02b8a645f29c2
sherpal8 <sherpalkorotana@icloud.com>
2019-12-22 17:42:31 +0000

    use Route along with values for  path & component properties

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Route } from "react-router-dom";
+import HomePage from "./home/HomePage.jsx";
+import AboutPage from "./about/AboutPage.jsx";
+
+const App = () => (
+  <div className="container-fluid">
+    <Route exact path="/" component={HomePage} />
+    <Route path="/about" component={AboutPage} />
+  </div>
+);
+
+export default App;
