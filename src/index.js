aec4cc914ca5dda56a78a23280b0504b1c6c6b01
sherpal8 <sherpalkorotana@icloud.com>
2019-12-22 17:14:24 +0000

    import index.css into entry app entry point file

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { render } from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./components/App";
+import "./index.css";
 
 render(
   <Router>
