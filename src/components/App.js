a92ee6e4a9b3fa0bf37c458da9e5e8fc32b6ad5c
sherpal8 <sherpalkorotana@icloud.com>
2019-12-22 18:01:00 +0000

    use Header presentational component inside App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,11 @@ import React from "react";
 import { Route } from "react-router-dom";
 import HomePage from "./home/HomePage.jsx";
 import AboutPage from "./about/AboutPage.jsx";
+import Header from "./common/Header.jsx";
 
 const App = () => (
   <div className="container-fluid">
+    <Header />
     <Route exact path="/" component={HomePage} />
     <Route path="/about" component={AboutPage} />
   </div>
