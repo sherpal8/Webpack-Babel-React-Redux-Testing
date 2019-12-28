aa7bf2bd2a940b7011616bf8451f27284b311bb7
sherpal8 <sherpalkorotana@icloud.com>
2019-12-28 15:58:27 +0000

    map dispatch loadAuthors() to props. Then, call loadAuthors() inside component did mount. Then, map authors and courses to props - with special consideration made to return the courses array into the desired format

--- src/components/courses/CoursesPage.jsx
+++ src/components/courses/CoursesPage.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import * as courseActions from "../../redux/actions/courseActions";
+import * as authorActions from "../../redux/actions/authorActions";
 import PropTypes from "prop-types";
 import { bindActionCreators } from "redux";
 import CourseList from "./CourseList.jsx";
@@ -9,7 +10,11 @@ class CoursesPage extends Component {
   componentDidMount() {
     this.props.actions
       .loadCourses()
-      .catch(error => alert("Loading courses failed" + error));
+      .catch(error => alert("Loading courses failed " + error));
+
+    this.props.actions
+      .loadAuthors()
+      .catch(error => alert("Loading authors failed " + error));
   }
 
   render() {
@@ -24,16 +29,32 @@ class CoursesPage extends Component {
 
 CoursesPage.propTypes = {
   courses: PropTypes.array.isRequired,
+  authors: PropTypes.array.isRequired,
   actions: PropTypes.object.isRequired
 };
 
-function mapStateToProps({ courses }) {
-  return { courses };
+function mapStateToProps({ courses, authors }) {
+  return {
+    courses:
+      authors.length === 0
+        ? []
+        : courses.map(course => {
+            return {
+              ...course,
+              authorName: authors.find(author => author.id === course.authorId)
+                .name
+            };
+          }),
+    authors
+  };
 }
 
 const mapDispatchToProps = dispatch => {
   return {
-    actions: bindActionCreators(courseActions, dispatch)
+    actions: {
+      loadCourses: bindActionCreators(courseActions.loadCourses, dispatch),
+      loadAuthors: bindActionCreators(authorActions.loadAuthors, dispatch)
+    }
   };
 };
 
